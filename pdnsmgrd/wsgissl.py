5d7699196c6572e58ee00781304e292d360ccd62
Christian Hofstaedtler <christian@hofstaedtler.name>
2013-09-13 11:06:00 +0200

    pdnsmgrd: pylint/pep8 cleanup

--- pdnsmgrd/wsgissl.py
+++ pdnsmgrd/wsgissl.py
@@ -14,7 +14,7 @@ class HTTPSMixIn:
     """A MixIn class for adding SSL to BaseHTTPServer.HTTPServer subclasses.
     This works with wsgiref.WSGIServer.
     """
-    def set_credentials( self, keypath=None, certpath=None):
+    def set_credentials(self, keypath=None, certpath=None):
         self.keypath = keypath
         self.certpath = certpath
 
@@ -24,8 +24,10 @@ class HTTPSMixIn:
         # Note: accessing self.* from here might not be thread-safe,
         # which could be an issue when using ThreadingMixIn.
         # In practice, the GIL probably prevents any trouble with read access.
-        ssock = ssl.wrap_socket( request,
-            keyfile=self.keypath, certfile=self.certpath, server_side=True)
+        ssock = ssl.wrap_socket(request,
+                                keyfile=self.keypath,
+                                certfile=self.certpath,
+                                server_side=True)
         self.RequestHandlerClass(ssock, client_address, self)
         ssock.close()
 
@@ -34,16 +36,12 @@ class SecureWSGIServer(HTTPSMixIn, WSGIServer):
     pass
 
 
-class SecureWSGIRequestHandler( WSGIRequestHandler):
+class SecureWSGIRequestHandler(WSGIRequestHandler):
     """An SSL-aware WSGIRequestHandler, which sets HTTPS environment variables.
     """
-    #xxx todo: set SSL_PROTOCOL, maybe others
-    def get_environ( self):
-        env = WSGIRequestHandler.get_environ( self)
-        if isinstance( self.request, ssl.SSLSocket):
+
+    def get_environ(self):
+        env = WSGIRequestHandler.get_environ(self)
+        if isinstance(self.request, ssl.SSLSocket):
             env['HTTPS'] = 'on'
         return env
-
-
-# ======================================================================
-# EOF

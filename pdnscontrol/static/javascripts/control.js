50049003369d08fd38f2b14bbc0b70572f184cb1
Christian Hofstaedtler <christian@hofstaedtler.name>
2014-02-19 00:57:21 +0100

    Add navigation bar

--- pdnscontrol/static/javascripts/control.js
+++ pdnscontrol/static/javascripts/control.js
@@ -270,6 +270,67 @@ ControlApp.directive('spinner', function() {
 
 
 ////////////////////////////////////////////////////////////////////////
+// Base UI
+////////////////////////////////////////////////////////////////////////
+
+ControlApp.factory('breadcrumbs', ['$rootScope', '$location', function($rootScope, $location) {
+  var breadcrumbs = [];
+  var breadcrumbsService = {};
+
+  //we want to update breadcrumbs only when a route is actually changed
+  //as $location.path() will get updated imediatelly (even if route change fails!)
+  $rootScope.$on('$routeChangeSuccess', function(event, current){
+    console.log('routeChangeSuccess');
+
+    var pathElements = $location.path().split('/'), result = [], i;
+    var breadcrumbPath = function (index) {
+      return '/' + (pathElements.slice(0, index + 1)).join('/');
+    };
+
+    pathElements.shift();
+    for (i=0; i<pathElements.length; i++) {
+      result.push({name: pathElements[i], path: breadcrumbPath(i)});
+    }
+
+    breadcrumbs = result;
+  });
+
+  breadcrumbsService.getAll = function() {
+    return breadcrumbs;
+  };
+
+  breadcrumbsService.getFirst = function() {
+    return breadcrumbs[0] || {};
+  };
+
+  return breadcrumbsService;
+}]);
+
+function NavCtrl($scope, breadcrumbs) {
+  $scope.breadcrumbs = breadcrumbs;
+  var filtered = [];
+  var rawCache;
+  $scope.breadcrumbs.filtered = function() {
+    var raw = breadcrumbs.getAll();
+    if (rawCache === raw) {
+      return filtered;
+    }
+
+    filtered.length = 0;
+    if (raw[0]) {
+      if (raw[0].name == 'server' && raw[1]) {
+        filtered.push(raw[1]);
+        if (raw[2] && raw[3]) {
+          filtered.push(raw[3]);
+        }
+      }
+    }
+    rawCache = raw;
+    return filtered;
+  };
+}
+
+////////////////////////////////////////////////////////////////////////
 // Servers
 ////////////////////////////////////////////////////////////////////////
 

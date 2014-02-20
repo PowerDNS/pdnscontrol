3bf5bcfe0a908923aad40944349820dcb4fa6507
Christian Hofstaedtler <christian@hofstaedtler.name>
2014-02-20 20:32:07 +0100

    Add manage.py reset-password

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
 from flask.ext.script import Manager
 from flask.ext.assets import ManageAssets
 from pdnscontrol import app, asset_env
+from pdnscontrol.manage import ManageResetPassword
 
 manager = Manager(app)
 manager.add_command("assets", ManageAssets(asset_env))
+manager.add_command("reset-password", ManageResetPassword())
 
 if __name__ == "__main__":
     manager.run()
-
---
+++ pdnscontrol/manage.py
@@ -0,0 +1,27 @@
+from flask.ext.script import Command, prompt_pass, Option
+from pdnscontrol import app
+from pdnscontrol.models import *
+from flask.ext.security.utils import encrypt_password
+
+class ManageResetPassword(Command):
+    "Reset the password of any user"
+
+    option_list = (
+        Option('email', help='E-Mail of the user to reset the password'),
+    )
+
+    def run(self, email):
+        u = user_datastore.find_user(email=email)
+        password = prompt_pass("New password for \"%s\"" % u.email)
+        password2 = prompt_pass("New password (repeat)")
+        if password != password2:
+            print "Passwords do not match."
+            return 1
+        u.password = encrypt_password(password)
+        db.session.add(u)
+        db.session.commit()
+        print "Password updated."
+        return 0
+
+#with app.app_context():
+

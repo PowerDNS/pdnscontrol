222bc68998243d87e76ee996a012ec59dde87694
Christian Hofstaedtler <christian@hofstaedtler.name>
2014-02-24 13:06:04 +0100

    When proxying graphite, show (internal) exceptions as SVG

--- pdnscontrol/views/graphite.py
+++ pdnscontrol/views/graphite.py
@@ -11,15 +11,35 @@ mod = Blueprint('graphite', __name__)
 @roles_required('stats')
 def graphite():
     params = dict((k,request.values.getlist(k)) for k in request.values.keys())
-    response = fetch_remote(
-        current_app.config['GRAPHITE_SERVER'],
-        method=request.method,
-        data=request.data,
-        accept=request.headers.get('Accept'),
-        params=params
-    )
-    return make_response((
-        response.content,
-        response.status_code,
-        {'Content-Type': response.headers.get('Content-Type')}
-    ))
+    try:
+        response = fetch_remote(
+            current_app.config['GRAPHITE_SERVER'],
+            method=request.method,
+            data=request.data,
+            accept=request.headers.get('Accept'),
+            params=params
+        )
+        return make_response((
+            response.content,
+            response.status_code,
+            {'Content-Type': response.headers.get('Content-Type')}
+        ))
+    except Exception as e:
+        width = request.values.get('width', '400')
+        height = request.values.get('height', '200')
+        image_tpl = """<?xml version="1.0" encoding="UTF-8"?>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 {width} {height}" width="{width}" height="{height}">
+    <text x="10" y="20" fill="red" font-size="15">
+        {error}
+    </text>
+</svg>"""
+        image = image_tpl.format(**{
+            'error': str(e),
+            'width': width,
+            'height': height
+        })
+        return make_response((
+            image,
+            500,
+            {'Content-Type': 'image/svg+xml'}
+        ))
